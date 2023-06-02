bbb9ac43b27335756539b3b52e4ddbd32d5d6688
heliosalesjr <heliosalesjr@gmail.com>
2023-06-02 15:46:36 -0300

    exploring the mui a little bit more

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import ProTip from './components/ProTip';
+
 import Copyright from './components/Copyright';
-import ControlledAccordions from './components/ControlledAccordions';
+
 import BasicCard from './components/BasicCard';
 
 export default function App() {
@@ -16,11 +16,11 @@ export default function App() {
         </Typography>
         
         
-        <BasicCard text='Palavrinha do Dia' wordz='bonitinha'/>
+        <BasicCard text='Uma palavra' wordz='samba' definition='dança de roda semelhante ao batuque, com dançarinos solistas e eventual presença da umbigada, difundida em todo o Brasil com variantes coreográficas e de acompanhamento instrumental [O samba urbano carioca foi tb., inicialmente, dança de roda.]'/>
 
-        <BasicCard text='Palavrinha da Tarde' wordz='querida!'/>
+        <BasicCard text='Qualquer palavra' wordz='dor' definition='sensação penosa, desagradável, produzida pela excitação de terminações nervosas sensíveis a esses estímulos, e classificada de acordo com o seu lugar, tipo, intensidade, periodicidade, difusão e caráter.'/>
 
-        <BasicCard text='Palavrinha da Noite' wordz='belissima'/>
+        <BasicCard text='Outra palavra' wordz='cores' definition='As cores são sensações visuais que nossos olhos captam quando há luz presente. Essas sensações são criadas pelas diferentes faixas luminosas que enxergamos. A origem da palavra "cor" vem do latim (color) e significa "cobrir" ou "ocultar".'/>
         
         
         <Copyright />
