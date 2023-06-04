4fc645daf2c764979cbceb19359ebce4cd834b5a
heliosalesjr <heliosalesjr@gmail.com>
2023-06-04 11:50:21 -0300

    css styling

--- src/components/BasicCard.jsx
+++ src/components/BasicCard.jsx
@@ -5,18 +5,17 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { createTheme } from '@mui/material/styles';
 import classes from './BasicCard.module.css';
 
 export default function BasicCard(props) {
 
   return (
-    <Card sx={{ minWidth: 275 }} className={classes.card}>
-      <CardContent >
+    <Card sx={{ minWidth: 275 }} >
+      <CardContent className={classes.card}>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           {props.text}
         </Typography >
-        <Typography variant="h5" component="div" >
+        <Typography variant="h5" component="div"  >
           {props.wordz}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
